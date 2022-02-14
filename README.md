78c8315b16f639efe3fc062677df76a213c8eb63
9rek1 <9rek199@gmail.com>
2022-02-15 02:23:03 +0900

    add README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+`~/.atcodertools.toml`
+
+```toml
+[codestyle]
+lang = 'cpp'
+template_file = '~/workspace/atcoder/template.cpp'
+workspace_dir = '~/workspace/atcoder/contests'
+```
--- template.cpp
+++ template.cpp
@@ -1,16 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-using pll = pair<ll, ll>;
-using vll = vector<ll>;
+using vl = vector<ll>;
 using vs = vector<string>;
-using vvll = vector<vll>;
-#define INF 1e18
+using vb = vector<bool>;
+using vvl = vector<vl>;
+using vvs = vector<vs>;
+using vvb = vector<vb>;
+using pl = pair<ll, ll>;
+const ll INF = 1e18;
+#define PB push_back
 #define FOR(i, m, n) for (ll i = m; i < (ll)n; ++i)
 #define REP(i, n) FOR(i, 0, n)
 #define RFOR(i, m, n) for (ll i = n - 1; i >= (ll)m; --i)
 #define RREP(i, n) RFOR(i, 0, n)
-#define EACH(i, v) for (auto& i : v)
+#define EACH(e, v) for (auto& e : v)
 #define ALL(v) v.begin(), v.end()
 #define RALL(v) v.rbegin(), v.rend()
 #define REVERSE(v) reverse(ALL(v))
@@ -21,28 +25,37 @@ using vvll = vector<vll>;
 #define MAXI(v) distance(v.begin(), max_element(ALL(v)))
 #define MIN(v) *min_element(ALL(v))
 #define MINI(v) distance(v.begin(), min_element(ALL(v)))
+#define CONCAT(a, b) a.insert(a.end(), ALL(b))
+void yesno(const bool& flag, const string& yes = "Yes",
+           const string& no = "No") {
+    cout << (flag ? yes : no) << '\n';
+}
+template <typename T> void print(const T& e) { cout << e << '\n'; }
 template <typename T> void print(const vector<T>& v) {
-    REP(i, v.size()) cout << v[i] << (i != (ll)v.size() - 1 ? ' ' : '\n');
+    for (ll i = 0; i < (ll)v.size(); ++i) {
+        cout << v[i];
+        cout << (i == (ll)v.size() - 1 ? '\n' : ' ');
+    }
 }
 template <typename T, typename S> void print(const pair<T, S>& p) {
     cout << p.first << ' ' << p.second << '\n';
 }
-template <typename T> void print(const T& x) { cout << x << '\n'; }
 template <class Head, class... Tail>
 void print(const Head& head, const Tail&... tail) {
     cout << head << ' ';
     print(tail...);
-}
-void yesno(bool flag, string yes = "Yes", string no = "No") {
-    print((flag ? yes : no));
 }
 void input() {}
 template <class Head, class... Tail> void input(Head& head, Tail&... tail) {
     cin >> head;
     input(tail...);
 }
-template <class T> bool chmin(T& a, T b) { return a > b && (a = b, true); }
-template <class T> bool chmax(T& a, T b) { return a < b && (a = b, true); }
+template <typename T> bool chmin(T& a, const T& b) {
+    return a > b && (a = b, true);
+}
+template <typename T> bool chmax(T& a, const T& b) {
+    return a < b && (a = b, true);
+}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -51,6 +64,6 @@ int main() {
 
     ll n, ans = 0;
     input(n);
-    // vll an(n); REP(i, n) input(an[i]);
+    // vl an(n); REP(i, n) input(an[i]);
     print(ans);
 }
